8761ce698d4387b1a8e36dfaecfc8267425b1874
Minivera <guillaume@manifold.co>
2020-03-05 17:27:28 -0500

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,55 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QTextBrowser>
+#include <QTextBlock>
+#include <QMessageBox>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    this->setWindowTitle("DENAS PRODUCT");
+    resize(700,550);
+    pal.setColor(QPalette::Background, Qt::gray);
+    setAutoFillBackground(true);
+    setPalette(pal);
+
+    ui->Back->resize(80,25);
+    ui->Top->resize(80,25);
+    ui->Down->resize(80,25);
+    ui->Open->resize(80,25);
+    ui->Off->resize(80,25);
+    ui->Screen->resize(250,200);
+
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_Open_clicked()
+{
+    if (ui->Screen->document()->blockCount() > 100)
+    {
+        ui->Screen->clear();
+    }
+    ui->Screen->document()->setMaximumBlockCount(100);
+    ui->Screen->append("Use Case1");
+    ui->Screen->append("Use Case2");
+    ui->Screen->append("Use Case3");
+    ui->Screen->append("Use Case4");
+    ui->Screen->append("Use Case5");
+    ui->Screen->append("Use Case6");
+
+}
+
+
+void MainWindow::on_Off_clicked()
+{
+    QApplication* e;
+     e->exit(0);
+
+
+}
---
+++ mainwindow.h
@@ -0,0 +1,30 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+
+    void on_Off_clicked();
+    void on_Open_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+    QPalette pal;
+    int row = 0;
+};
+
+#endif // MAINWINDOW_H
